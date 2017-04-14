c320f8e388f8e88e23c0c294b68b0fb086a40626
Nese M <nese_m@hotmail.co.uk>
2017-04-14 20:04:54 +0100

    changed layout

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,29 @@
-##The pair of functions below, when used correctly (the output of the first one used as the argument to the second), return the 
-##inverse of a matrix, and "cache" this result in a "matrix" object (stored in function 1), which can then be retrieved when needed 
-##(using function 2), making matrix inverse computations much faster.
+######################
+######################
+
+##The pair of functions below, when used correctly (the output of the first 
+##one used as the argument to the second), return the inverse of a matrix, 
+##and "cache" this result in a "matrix" object (stored in function 1), which 
+##can then be retrieved when needed (using function 2), making matrix inverse
+##computations much faster.
 ##
-##The makeCacheMatrix() function takes an invertible matrix as its argument, and returns a list of named elements 
-##(which are outputs from the accessor and mutator methods used within the body of the function). You can then pass the output of this
-##function as an argument to the second function, cacheSolve(), which then calculates and stores the inverse of the initial input matrix 
-##in the first function, and returns this inverse as an output.
+##The makeCacheMatrix() function takes an invertible matrix as its argument, 
+##and returns a list of named elements (which are outputs from the accessor 
+##and mutator methods used within the body of the function). You can then pass
+##the output of thismfunction as an argument to the second function, 
+##cacheSolve(), which then calculates and stores the inverse of the initial 
+##input matrix in the first function, and returns this inverse as an output.
 ##
-##Then whenever cacheSolve() is called again using the same argument (until a new matrix is set using the set function), it will immediately 
-##retrieve the inverse without needing to recalculate it.
+##Then whenever cacheSolve() is called again using the same argument (until a 
+##new matrix is set using the set function), it will immediately retrieve the 
+##inverse without needing to recalculate it.
 ##
-##makeCacheMatrix(), uses two key variables, x and m; x stores the input matrix data, and m stores the inverse of that matrix (though it is 
-##initialized as 0).
+##makeCacheMatrix(), uses two key variables, x and m; x stores the input matrix 
+##data, and m stores the inverse of that matrix (though it is initialized as 0).
 ##
-##If there is already an inverse cached in m, then the value of m cached in the memory of the object is cleared whenever x is reset, forcing 
-##subsequent calls to cacheSolve() to recalculate the inverse.
+##If there is already an inverse cached in m, then the value of m cached in the 
+##memory of the object is cleared whenever x is reset, forcing subsequent calls 
+##to cacheSolve() to recalculate the inverse.
 
 
 #This function creates a "matrix" object that can cache its inverse.
